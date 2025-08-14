131d03fbc7a4ad07265b4df7a6f9b0b67aac4a95
Roei Baruch <roei.b46@gmail.com>
2025-08-14 20:21:01 +0300

    Init

---
+++ IbiProductStore/IbiProductStoreTests/IbiProductStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  IbiProductStoreTests.swift
+//  IbiProductStoreTests
+//
+//  Created by Roei Baruch on 14/08/2025.
+//
+
+import Testing
+@testable import IbiProductStore
+
+struct IbiProductStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
